98649487d6094d8b53652cbce2d2ba0c62ea1dfb
njang <neo.w.jang@gmail.com>
2018-01-14 14:28:59 -0600

    Complete S1S2 by adding index route

--- server.js
+++ server.js
@@ -11,37 +11,39 @@ app.use(express.static(__dirname + '/public'));
 /************
  * DATABASE *
  ************/
+var db = require('./models');
+
 
 /* hard-coded data */
-var albums = [];
-albums.push({
-              _id: 132,
-              artistName: 'Nine Inch Nails',
-              name: 'The Downward Spiral',
-              releaseDate: '1994, March 8',
-              genres: [ 'industrial', 'industrial metal' ]
-            });
-albums.push({
-              _id: 133,
-              artistName: 'Metallica',
-              name: 'Metallica',
-              releaseDate: '1991, August 12',
-              genres: [ 'heavy metal' ]
-            });
-albums.push({
-              _id: 134,
-              artistName: 'The Prodigy',
-              name: 'Music for the Jilted Generation',
-              releaseDate: '1994, July 4',
-              genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
-            });
-albums.push({
-              _id: 135,
-              artistName: 'Johnny Cash',
-              name: 'Unchained',
-              releaseDate: '1996, November 5',
-              genres: [ 'country', 'rock' ]
-            });
+// var albums = [];
+// albums.push({
+//               _id: 132,
+//               artistName: 'Nine Inch Nails',
+//               name: 'The Downward Spiral',
+//               releaseDate: '1994, March 8',
+//               genres: [ 'industrial', 'industrial metal' ]
+//             });
+// albums.push({
+//               _id: 133,
+//               artistName: 'Metallica',
+//               name: 'Metallica',
+//               releaseDate: '1991, August 12',
+//               genres: [ 'heavy metal' ]
+//             });
+// albums.push({
+//               _id: 134,
+//               artistName: 'The Prodigy',
+//               name: 'Music for the Jilted Generation',
+//               releaseDate: '1994, July 4',
+//               genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
+//             });
+// albums.push({
+//               _id: 135,
+//               artistName: 'Johnny Cash',
+//               name: 'Unchained',
+//               releaseDate: '1996, November 5',
+//               genres: [ 'country', 'rock' ]
+//             });
 
 
 
@@ -55,6 +57,12 @@ albums.push({
 
 app.get('/', function homepage (req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/albums', function albumsIndex(req, res) {
+  db.Album.find({}, function(err, albums) {
+    res.json(albums);
+  });
 });
 
 
